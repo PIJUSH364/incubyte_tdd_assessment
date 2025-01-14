2a96bf17deb239620420a98aa5a74b535568f9ad
Pijush Ray mondal <62095229+PIJUSH364@users.noreply.github.com>
2025-01-14 22:52:19 +0530

    Add test case to validate support for different delimiters in add function

--- strCalculator.test.js
+++ strCalculator.test.js
@@ -16,4 +16,6 @@ test('should handle multiple numbers', () => {
 test('should handle new lines between numbers', () => {
     expect(add("1\n2,3")).toBe(6);
 });
-
+test('should support different delimiters', () => {
+    expect(add("//;\n1;2")).toBe(3);
+});
