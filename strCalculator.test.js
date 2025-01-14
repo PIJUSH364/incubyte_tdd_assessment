8bc17b50ba52a7d86643a88ee45fa3bafaf5d112
Pijush Ray mondal <62095229+PIJUSH364@users.noreply.github.com>
2025-01-14 22:42:45 +0530

    Add test case to validate handling of new lines between numbers in add function

--- strCalculator.test.js
+++ strCalculator.test.js
@@ -13,3 +13,7 @@ test('should return the sum of two numbers', () => {
 test('should handle multiple numbers', () => {
     expect(add("1,2,3,4")).toBe(10);
 });
+test('should handle new lines between numbers', () => {
+    expect(add("1\n2,3")).toBe(6);
+});
+
