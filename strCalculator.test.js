540bf172b19ceefdbad7a64fdacd6c2b2f505105
Pijush Ray mondal <62095229+PIJUSH364@users.noreply.github.com>
2025-01-14 22:55:34 +0530

    Add test case to display all negative numbers in exception message

--- strCalculator.test.js
+++ strCalculator.test.js
@@ -23,3 +23,6 @@ test('should throw an exception for negative numbers', () => {
     expect(() => add("1,-2")).toThrow('negative numbers not allowed: -2');
 });
 
+test('should show all negative numbers in the exception message', () => {
+    expect(() => add("1,-2,-3")).toThrow('negative numbers not allowed: -2, -3');
+});
